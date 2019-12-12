52a52d3a2e96ba4cfff833986623a96553d6088b
nicoleputois <nicoleputois@hotmail.com>
2019-12-12 22:20:57 +0100

    Ajoutez des fichiers projet.

---
+++ Microsoft.DotNet.Build.VstsBuildsApi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Microsoft.DotNet.Build.VstsBuildsApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+    }
+}
